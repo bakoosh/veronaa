31cf1e1149ce5af37d509befd7aa4815c443902c
samdev-87 <kalauov.s.t@gmail.com>
2024-06-18 11:04:06 +0500

    main page removed @vite

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
 </head>
 <body>
 <h1 class="text-9xl font-bold underline">
-
+    Hello world!
 </h1>
 </body>
 </html>
